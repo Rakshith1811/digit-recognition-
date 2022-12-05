dbc65310600d6c0cbfb66ee6528f357709beea18
Rakshith <118827661+Rakshith1811@users.noreply.github.com>
2022-12-05 22:43:11 +0530

    Add files via upload

---
+++ digit_recognition/app2.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import cv2
+from tensorflow.keras.models import load_model
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+
+
+model = load_model('digit_recognition_project.h5')
+
+
+SIZE = 200
+mode = st.checkbox("Draw (or Delete)?", True)
+canvas_result = st_canvas(
+    fill_color='#000000',
+    stroke_width=20,
+    stroke_color='#FFFFFF',
+    background_color='#000000',
+    width=SIZE,
+    height=SIZE,
+    drawing_mode="freedraw",
+    key='canvas')
+#  if mode else "transform"
+
+if canvas_result.image_data is not None:
+    img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+    rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+    st.write('Model Input')
+    st.image(rescaled)
+
+if st.button('Predict'):
+    test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    val = model.predict(test_x.reshape(1,28, 28, 1))
+    st.write(f'result: {np.argmax(val[0])}')
+    st.bar_chart(val[0])
